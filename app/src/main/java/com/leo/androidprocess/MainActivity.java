b0938ac1a3e5b53ac1cccef213c928d296d24198
piece-the-world <tao.chao.chao@gmail.com>
2016-05-12 15:58:48 +0800

    demo for native executable

--- app/src/main/java/com/leo/androidprocess/MainActivity.java
+++ app/src/main/java/com/leo/androidprocess/MainActivity.java
@@ -5,9 +5,16 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.leo.androidprocess.utils.UnixExecUtil;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        init();
+    }
+
+    private void init() {
+        findViewById(R.id.bt1).setOnClickListener(this);
+        findViewById(R.id.bt2).setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt1:
+                UnixExecUtil.initNativeExecutable(this, "iptables");
+                break;
+            case R.id.bt2:
+                String executablePath = getApplicationContext().getFilesDir().getAbsolutePath() + File.separator + "iptables -h";
+                String output = UnixExecUtil.exec(executablePath);
+                ((TextView) findViewById(R.id.tv1)).setText(output);
+                break;
+        }
     }
 }
