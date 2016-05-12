a2449595d42ed5a9ecda9ed20fe7048686643687
piece-the-world <tao.chao.chao@gmail.com>
2016-05-12 15:51:10 +0800

    init commit

--- AndroidProcess/app/src/main/java/process/leo/com/androidprocess/log/Tags.java
+++
@@ -1,9 +0,0 @@
-package process.leo.com.androidprocess.log;
-
-/**
- * Created by leo on 16-4-2.
- */
-public class Tags {
-    public static final String TEST = "test";
-    public static final String EXEC = "executable";
-}
